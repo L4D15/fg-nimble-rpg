8f2d51a14bd18540fb180c447cc2f56c27e5baac
Jose Ladislao Lainez Ortega <thedragonladis@gmail.com>
2026-03-02 20:33:53 +0100

    Add support for drag & drop ancestries

--- campaign/scripts/char_main.lua
+++ campaign/scripts/char_main.lua
@@ -2,3 +2,24 @@
 function onInit()
 
 end
+
+
+-- Listen for anything dropped into the window
+function onDrop(x, y, draginfo)
+    if draginfo.isType("shortcut") then
+        local sClass, sRecord = draginfo.getShortcutData()
+        
+        if sClass == "reference_ancestry" then
+            onAncestryDropped(sClass, sRecord)
+            -- Return true so FG know we have processed this event
+            return true
+        end
+    end
+end
+
+function onAncestryDropped(sClass, sRecord)
+    local sAncestryName = DB.getValue(DB.getPath(sRecord, "name"), "")
+
+    ancestry.setValue(sAncestryName)
+    ancestrylink.setValue(sClass, sRecord)
+end
